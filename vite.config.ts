bc50a904c70ddcde7a89d50152f9936c435c80a5
BoBoooooo <17746714@qq.com>
2021-03-14 21:36:46 +0800

    chore: 引入vite-plugin-html插件

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,20 @@ import vue from '@vitejs/plugin-vue';
 import jsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-
+import { injectHtml } from 'vite-plugin-html';
 export default defineConfig({
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
     },
   },
-  plugins: [vue(), jsx()],
+  plugins: [
+    vue(),
+    jsx(),
+    injectHtml({
+      injectData: {
+        title: 'vite_vue3_demo',
+      },
+    }),
+  ],
 });
